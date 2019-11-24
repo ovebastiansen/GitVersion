d449fae6ea388173c1cf22e14a1c2c3fcf4d223f
Ove Bastiansen <obast@nets.eu>
2019-11-24 12:49:44 +0100

    Marius version

---
+++ src/GitVersionCore.Tests/IntegrationTests/EasyScenarios.cs
@@ -0,0 +1,208 @@
+namespace GitVersionCore.Tests.IntegrationTests
+{
+    using System.Collections.Generic;
+    using GitTools.Testing;
+    using GitVersion;
+    using GitVersion.Configuration;
+    using LibGit2Sharp;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EasyScenarios : TestBase
+    {
+        static Config config = new Config
+        {
+            //VersioningMode = VersioningMode.ContinuousDeployment,
+            Branches =
+            {
+                { "master", new BranchConfig
+                    {
+                        Tag = "beta",
+                        Regex = "^master$",
+                        TracksReleaseBranches = true, // turning this on will make the master version from when the last release branch was created,
+                        Increment = IncrementStrategy.Minor
+                    }
+                },
+                { "release", new BranchConfig
+                    {
+                        Tag = "",
+                        Regex = "^releases?[/-]"
+                    }
+                },
+                { "feature", new BranchConfig
+                    {
+                        Tag = "useBranchName",
+                        Regex = "^features?[/-]"
+                    }
+                },
+                { "bugfix", new BranchConfig
+                    {
+                        Tag = "useBranchName",
+                        Regex = "^bugfix(es)?[/-]",
+                        SourceBranches = new List<string> { "master", "release" }
+                    }
+                }
+            }
+        };
+
+
+        [Test]
+        public void Master_has_a_minor_version_number_which_is_plus_one_since_last_release_branch_when_not_merged()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("master");
+            fixture.Repository.MakeACommit();
+
+            fixture.AssertFullSemver(config, "1.27.0-beta.1+1");
+        }
+
+        [Test]
+        public void Merge_release_branch_back_to_master()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("release/1.26");
+            fixture.Repository.MakeACommit();
+
+            fixture.Checkout("master");
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.MergeNoFF("release/1.26");
+
+            fixture.AssertFullSemver(config, "1.27.0-beta.1+3");
+        }
+
+        [Test]
+        public void Bugfix_on_release_branch()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("release/1.26");
+            fixture.Repository.MakeACommit();
+            fixture.AssertFullSemver(config, "1.26.0+1");
+            fixture.ApplyTag("1.26");
+
+            fixture.Repository.CreateBranch("bugfix/on-release-branch");
+            fixture.Checkout("bugfix/on-release-branch");
+            fixture.Repository.MakeACommit();
+            fixture.AssertFullSemver(config, "1.26.0-on-release-branch.1+1");
+
+            fixture.Repository.MakeACommit();
+            fixture.AssertFullSemver(config, "1.26.0-on-release-branch.1+2");
+
+            fixture.Checkout("release/1.26");
+            fixture.Repository.MergeNoFF("bugfix/on-release-branch");
+
+            fixture.AssertFullSemver(config, "1.26.0+4");
+        }
+
+        [Test]
+        public void Bugfix_on_master_branch()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("release/1.26");
+            fixture.ApplyTag("1.26");
+
+            fixture.Checkout("master");
+            fixture.AssertFullSemver(config, "1.26.0");
+            fixture.Repository.MakeACommit();
+            fixture.AssertFullSemver(config, "1.27.0-beta.1+1");
+
+            fixture.Repository.CreateBranch("bugfix/on-master");
+            fixture.Checkout("bugfix/on-master");
+            fixture.Repository.MakeACommit();
+            fixture.AssertFullSemver(config, "1.27.0-on-master.1+2");
+        }
+
+        [Test]
+        public void Pull_request_of_feature_branch_to_master_both_feature_and_master_has_commits()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("master");
+
+            fixture.Repository.CreateBranch("feature/some-new-feature");
+
+            fixture.Checkout("master");
+            fixture.Repository.MakeACommit();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.MakeACommit();
+
+            fixture.Checkout("feature/some-new-feature");
+            fixture.Repository.MakeACommit();
+            fixture.Repository.MakeACommit();
+
+            fixture.Checkout("master");
+            fixture.Repository.MergeNoFF("feature/some-new-feature");
+
+            fixture.AssertFullSemver(config, "1.27.0-beta.1+7");
+        }
+
+        [Test]
+        public void Feature_branch_from_master_no_commits()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("master");
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.CreateBranch("feature/some-new-feature");
+            fixture.Checkout("feature/some-new-feature");
+
+            fixture.AssertFullSemver(config, "1.27.0-some-new-feature.1+1");
+        }
+
+        [Test]
+        public void Feature_branch_from_master_with_commits()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("master");
+            fixture.Repository.MakeACommit();
+
+            fixture.Repository.CreateBranch("feature/some-new-feature");
+            fixture.Checkout("feature/some-new-feature");
+            fixture.Repository.MakeACommit();
+
+            fixture.AssertFullSemver(config, "1.27.0-some-new-feature.1+2");
+        }
+
+
+        [Test]
+        public void Release_branch_from_master_no_commits()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("release/1.26");
+
+            fixture.AssertFullSemver(config, "1.26.0+0");
+        }
+
+        [Test]
+        public void Release_branch_from_master_with_commits()
+        {
+            using var fixture = new EmptyRepositoryFixture();
+            fixture.Repository.MakeACommit();
+            fixture.Repository.CreateBranch("release/1.26");
+            fixture.Checkout("release/1.26");
+            fixture.Repository.MakeACommit();
+
+            fixture.AssertFullSemver(config, "1.26.0+1");
+        }
+
+    }
+}
